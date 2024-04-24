9334f09f3f3c51ae84ed3fcf62d1be98e91aaaf2
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-04-24 17:43:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Ramsha   8/3/2024
+let famous_person = "Bill_gates";
+let quote = " said, your most unhappay customers are your greatest source of learning";
+console.log(famous_person, quote); // Will print famous_person name (Bill gates) and his quote.
+export {};
