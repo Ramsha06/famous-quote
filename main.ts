b520ee0983361e84de3ae81808348495e8af076c
Ramsha06 <164333955+Ramsha06@users.noreply.github.com>
2024-04-24 17:42:54 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,5 @@
+                    // Ramsha   8/3/2024
+
+let famous_person:string= "Bill_gates";
+let quote : string = " said, your most unhappay customers are your greatest source of learning";
+console.log(famous_person ,quote);  // Will print famous_person name (Bill gates) and his quote.
